cbd90d7876b115c53731fec992626fd9dacf55ff
“lucerodavi” <lucerodavi96@gmail.com>
2020-11-11 19:38:52 -0800

    appfairy: Migrate

--- src/views/index.js
+++ src/views/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 
-export { default as ContactFormView } from './ContactFormView'
+export { default as ArticleBoxView } from './ArticleBoxView'
 export { default as UnauthorizedView } from './UnauthorizedView'
 export { default as NotFoundView } from './NotFoundView'
 export { default as AboutView } from './AboutView'
