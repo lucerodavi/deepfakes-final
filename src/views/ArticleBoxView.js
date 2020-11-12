d69ca9e143a115ddb539775a13fe5e5db4225e24
“lucerodavi” <lucerodavi96@gmail.com>
2020-11-11 22:16:44 -0800

    appfairy: Migrate

--- src/views/ArticleBoxView.js
+++ src/views/ArticleBoxView.js
@@ -46,7 +46,9 @@ class ArticleBoxView extends React.Component {
 
   render() {
     const proxies = Controller !== ArticleBoxView ? transformProxies(this.props.children) : {
+      'article-box': [],
       'article': [],
+      'submit': [],
     }
 
     return (
@@ -57,22 +59,24 @@ class ArticleBoxView extends React.Component {
           @import url(/css/deepfakes.webflow.css);
         ` }} />
         <span className="af-view">
-          <div className="w-container">
-            <h2>Search accuracy by Text</h2>
-            <p>Copy the text from any article you would like to compare to our Golden Sources and click on the Submit Button</p>
-            <div className="w-form">
-              <form id="wf-form-Contact-Form" name="wf-form-Contact-Form" data-name="Contact Form" method="post" redirect="/results" data-redirect="/results" className="af-class-form">
-                <div className="af-class-contact-form-grid-2">
-                  <div id="w-node-0e9e3bc05e7c-5cb757be" className="af-class-div-block-2">{map(proxies['article'], props => <textarea data-name maxLength={5000} required placeholder="Enter your text here. . ." id="article-box" {...{...props, className: `af-class-textarea w-input ${props.className || ''}`}}>{props.children}</textarea>)}</div>
-                </div><input type="submit" defaultValue="Submit" data-wait="Please wait..." ap-sock="submit" className="af-class-submit-button w-button" />
-              </form>
-              <div className="w-form-done">
-                <div>Thank you! Your submission has been received!</div>
+          <div className="af-class-contact-form-2">
+            {map(proxies['article-box'], props => <div {...{...props, className: `w-container ${props.className || ''}`}}>{createScope(props.children, proxies => <React.Fragment>
+              <h2>Search accuracy by Text</h2>
+              <p>Copy the text from any article you would like to compare to our Golden Sources and click on the Submit Button</p>
+              <div className="w-form">
+                <form id="wf-form-Contact-Form" name="wf-form-Contact-Form" data-name="Contact Form" method="post" redirect="/results" data-redirect="/results" className="af-class-form">
+                  <div className="af-class-contact-form-grid-2">
+                    <div id="w-node-0e9e3bc05e7c-5cb757be" className="af-class-div-block-2" />
+                  </div>{map(proxies['article'], props => <input type="text" maxLength={256} name="textarticle" data-name="textarticle" placeholder="Enter your text here. . ." id="textarticle" required {...{...props, className: `af-class-text-field-2 w-input ${props.className || ''}`}}>{props.children}</input>)}{map(proxies['submit'], props => <input type="submit" value="Submit" data-wait="Please wait..." {...{...props, className: `af-class-submit-button w-button ${props.className || ''}`}}>{props.children}</input>)}
+                </form>
+                <div className="w-form-done">
+                  <div>Thank you! Your submission has been received!</div>
+                </div>
+                <div className="w-form-fail">
+                  <div>Oops! Something went wrong while submitting the form.</div>
+                </div>
               </div>
-              <div className="w-form-fail">
-                <div>Oops! Something went wrong while submitting the form.</div>
-              </div>
-            </div>
+            </React.Fragment>)}</div>)}
           </div>
         </span>
       </span>
